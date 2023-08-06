c4104b58454dfb596cc42661fdf02722f3146abb
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-08-06 13:18:51 -0300

    Listing Users GET Request

--- src/components/Usuarios/Usuarios.js
+++ src/components/Usuarios/Usuarios.js
@@ -1,33 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import AdicionarUsuario from '../AdicionarUsuario/AdicionarUsuario'
-import Usuario from '../Usuario/Usuario'
+import AdicionarUsuario from "../AdicionarUsuario/AdicionarUsuario";
+import Usuario from "../Usuario/Usuario";
 
 class Usuarios extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      usuarios: [
-        { id: 1, nome: 'João', sobrenome: 'Silva', email: 'joao@mail.com' },
-        { id: 2, nome: 'Maria', sobrenome: 'Santos', email: 'maria@mail.com' }
-      ]
-    }
+      usuarios: [],
+    };
 
-    this.adicionarUsuario = this.adicionarUsuario.bind(this)
+    this.adicionarUsuario = this.adicionarUsuario.bind(this);
   }
 
   adicionarUsuario(usuario) {
-    const usuarios = [...this.state.usuarios, usuario]
-    this.setState({ usuarios: usuarios })
+    const usuarios = [...this.state.usuarios, usuario];
+    this.setState({ usuarios: usuarios });
   }
 
   removerUsuario(usuario) {
-    if (window.confirm(`Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`)) {
-      let usuarios = this.state.usuarios
-      usuarios = usuarios.filter(x => x.id !== usuario.id)
-      this.setState({ usuarios: usuarios })
+    if (
+      window.confirm(
+        `Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`
+      )
+    ) {
+      let usuarios = this.state.usuarios;
+      usuarios = usuarios.filter((x) => x.id !== usuario.id);
+      this.setState({ usuarios: usuarios });
     }
+  }
+
+  async componentDidMount() {
+    const response = await fetch("https://reqres.in/api/users");
+    const json = await response.json();
+    const data = await json.data;
+
+    const newData = data.map((element) => {
+      return {
+        id: element.id,
+        nome: element.first_name,
+        sobrenome: element.last_name,
+        email: element.email,
+      };
+    });
+
+    this.setState({ usuarios: newData });
   }
 
   render() {
@@ -35,15 +52,16 @@ class Usuarios extends Component {
       <>
         <AdicionarUsuario adicionarUsuario={this.adicionarUsuario} />
 
-        {this.state.usuarios.map(usuario => (
-          <Usuario key={usuario.id}
+        {this.state.usuarios.map((usuario) => (
+          <Usuario
+            key={usuario.id}
             usuario={usuario}
             removerUsuario={this.removerUsuario.bind(this, usuario)}
           />
         ))}
       </>
-    )
+    );
   }
 }
 
-export default Usuarios
+export default Usuarios;
